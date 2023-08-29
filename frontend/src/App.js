b547cfceca6c0660dbe3c16452cdf368f5e13a09
Aayush Adhikari <adhikariaayush37@gmail.com>
2023-08-29 13:31:42 +0545

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -8,11 +8,31 @@ import Contact from './pages/Contact';
 import Features from './pages/Features';
 import './styles.css';
 
-
 function App() {
+    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const [theme, setTheme] = useState(userPrefersDark ? 'dark' : 'light');
+
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.documentElement.classList.add('dark-theme');
+            document.documentElement.classList.remove('light-theme');
+        } else {
+            document.documentElement.classList.add('light-theme');
+            document.documentElement.classList.remove('dark-theme');
+        }
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light');
+    };
+
     return (
         <Router>
             <Navbar />
+            <button className="switch-theme-btn" onClick={toggleTheme}>
+    Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+</button>
+
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
